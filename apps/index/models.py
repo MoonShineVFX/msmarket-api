4c01062d21e8bf14671a407484625a503c2e4425
lomit <1234another@gmail.com>
2022-01-10 15:48:04 +0800

    Add delete,replace file signals

--- apps/index/models.py
+++ apps/index/models.py
@@ -3,6 +3,10 @@ from ..user.models import EditorBaseModel, CreatorBaseModel
 from ..product.models import Product
 from ..storage import PublicGoogleCloudStorage
 
+from google.cloud import storage, exceptions
+from django.dispatch import receiver
+from django.conf import settings
+
 
 class Banner(CreatorBaseModel):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
@@ -22,3 +26,43 @@ class AboutUs(EditorBaseModel):
     model_count = models.IntegerField(default=0)
     format_count = models.IntegerField(default=0)
     render_count = models.IntegerField(default=0)
+
+
+@receiver(models.signals.post_delete, sender=Tutorial)
+def auto_delete_file(sender, instance, **kargs):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(settings.GS_PUBLIC_BUCKET_NAME)
+    blob = bucket.blob(instance.file.name)
+    try:
+        blob.delete()
+    except Exception as e:
+        print(e)
+
+
+@receiver(models.signals.pre_save, sender=AboutUs)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `File` object is updated
+    with new file.
+    """
+    if not instance.id:
+        return False
+
+    old_file = None
+    new_file = None
+    try:
+        Model = type(instance)
+        old_file = Model.objects.get(id=instance.id).image
+        new_file = instance.image
+    except models.ObjectDoesNotExist:
+        return False
+
+    if not old_file:
+        return False
+
+    if not old_file == new_file:
+        try:
+            old_file.storage.delete(name=old_file.name)
+        except exceptions.NotFound as e:
+            print(e)
